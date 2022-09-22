bbc98b99bf47cab64ced2215cc2c60de4f12b2c5
Harry Hughes <harryhughes14@hotmail.com>
2022-09-22 14:35:37 +0100

    Add code that makes the camera follow the cursor

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -4,42 +4,51 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour
 {
-    private Camera Camera;
     // Start is called before the first frame update
     void Start()
     {
-        Camera = GetComponent<Camera>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Translates the camera
         if (Input.GetKey(KeyCode.LeftShift))
         {
             if (Input.GetKey(KeyCode.W))
             {
-                Camera.transform.position += new Vector3(0, 0, 0.01f);
+                transform.position += new Vector3(0, 0, 0.01f);
             }
             if (Input.GetKey(KeyCode.S))
             {
-                Camera.transform.position += new Vector3(0, 0, -0.01f);
+                transform.position += new Vector3(0, 0, -0.01f);
             }
             if (Input.GetKey(KeyCode.A))
             {
-                Camera.transform.position += new Vector3(-0.01f, 0, 0);
+                transform.position += new Vector3(-0.01f, 0, 0);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                Camera.transform.position += new Vector3(0.01f, 0, 0);
+                transform.position += new Vector3(0.01f, 0, 0);
             }
             if (Input.GetKey(KeyCode.E))
             {
-                Camera.transform.position += new Vector3(0, 0.01f, 0);
+                transform.position += new Vector3(0, 0.01f, 0);
             }
             if (Input.GetKey(KeyCode.Q))
             {
-                Camera.transform.position += new Vector3(0, -0.01f, 0);
+                transform.position += new Vector3(0, -0.01f, 0);
             }
         }
+
+        // Camera follows the mouse 
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            float mouseX = (Input.mousePosition.x / Screen.width) - 0.5f;
+            float mouseY = (Input.mousePosition.y / Screen.height) - 0.5f;
+            transform.localRotation = Quaternion.Euler(new Vector4(-1f * (mouseY * 180f), mouseX * 360f, transform.localRotation.z));
+        }
+        
     }
 }
